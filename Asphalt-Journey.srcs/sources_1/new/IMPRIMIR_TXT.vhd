57a473fabf36c9fd7dadfc7a698407f0c8742abd
cesar-4 <cesar.alonso.romero@alumnos.upm.es>
2024-12-16 20:08:44 +0100

    IMPRIMIR_TXT funcional v2
    Probado con testbench, funciona

--- Asphalt-Journey.srcs/sources_1/new/IMPRIMIR_TXT.vhd
+++ Asphalt-Journey.srcs/sources_1/new/IMPRIMIR_TXT.vhd
@@ -1,23 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 16.12.2024 10:31:02
--- Design Name: 
--- Module Name: IMPRIMIR_TXT - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
